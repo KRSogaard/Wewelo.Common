36e05966840bd7205e55303920f55b33acc2caf6
Kasper Rune Søgaard <kasper@sogaard.us>
2017-05-24 22:53:55 -0700

    Added Validator and TaskHelper

---
+++ Wewelo.Common/Tasks/TaskHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wewelo.Common.Tasks
+{
+    public static class TaskHelper
+    {
+
+        public static Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            return task.IsCompleted
+                ? task
+                : task.ContinueWith(
+                    completedTask => completedTask.GetAwaiter().GetResult(),
+                    cancellationToken,
+                    TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default);
+        }
+    }
+}
---
+++ Wewelo.Common/Validator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Wewelo.Common
+{
+    public static class Validator
+    {
+        public static void NonNull(string field, Object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentException($"Field \"{field}\" is required.");
+            }
+        }
+        public static void NonEmpty(string field, String obj)
+        {
+            NonNull(field, obj);
+            if (String.IsNullOrWhiteSpace(obj))
+            {
+                throw new ArgumentException($"Field \"{field}\" is can not be empty.");
+            }
+        }
+    }
+}
