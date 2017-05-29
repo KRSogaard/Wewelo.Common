768a78be8510d0d870b80f2775845f324fa01afd
Kasper Rune Søgaard <kasper@sogaard.us>
2017-05-28 22:29:39 -0700

    Added Compare helper

---
+++ Wewelo.Common.Tests/CompareTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Wewelo.Common.Tests
+{
+    public class CompareTests
+    {
+        [Fact]
+        public void TestNullLists()
+        {
+            Assert.True(Compare.CompareList((List<string>)null, (List<string>)null));
+        }
+
+        [Fact]
+        public void TestNullAndChangedists()
+        {
+            Assert.False(Compare.CompareList((List<string>)null, new List<string>()
+            {
+                "String1",
+                "String2"
+            }));
+        }
+
+        [Fact]
+        public void TestChangedListsAndNull()
+        {
+            Assert.False(Compare.CompareList(new List<string>()
+            {
+                "String1",
+                "String2"
+            }, (List<string>)null));
+        }
+
+        [Fact]
+        public void TestTwoLists()
+        {
+            Assert.True(Compare.CompareList(new List<string>()
+            {
+                "String1",
+                "String2"
+            }, new List<string>()
+            {
+                "String1",
+                "String2"
+            }));
+        }
+
+        [Fact]
+        public void TestOneItemLists()
+        {
+            Assert.True(Compare.CompareList(new List<string>()
+            {
+                "String1"
+            }, new List<string>()
+            {
+                "String1"
+            }));
+        }
+
+        [Fact]
+        public void TestDiffrentOneItemLists()
+        {
+            Assert.False(Compare.CompareList(new List<string>()
+            {
+                "String1"
+            }, new List<string>()
+            {
+                "String2"
+            }));
+        }
+
+        [Fact]
+        public void TestChangedTwoLists()
+        {
+            Assert.False(Compare.CompareList(new List<string>()
+            {
+                "String1",
+                "String2"
+            }, new List<string>()
+            {
+                "String1",
+                "String3"
+            }));
+        }
+    }
+}
---
+++ Wewelo.Common/Compare.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wewelo.Common
+{
+    public static class Compare
+    {
+        public static bool CompareList<T>(List<T> original, List<T> current)
+        {
+            if (original == null && current != null ||
+                original != null && current == null)
+                return false;
+            if (original == null && current == null)
+                return true;
+
+            if (original.Count != current.Count)
+                return false;
+
+            for (int i = 0; i < original.Count; i++)
+            {
+                if (original[i] == null)
+                {
+                    if (current[i] != null)
+                        return false;
+                } else if (!original[i].Equals(current[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
